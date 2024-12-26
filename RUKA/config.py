070140e058b3ff50c3276ae4b197facaa9e75eb2
Sandalivihansa <114420339+Sandalivihansa@users.noreply.github.com>
2024-12-26 08:23:32 -0800

    Update config.py

--- RUKA/config.py
+++ RUKA/config.py
@@ -12,25 +12,25 @@ def get_user_list(key):
 
 class Config:
     # Basic Bot Configuration
-    TOKEN = os.getenv("TOKEN", "6208314828:AAFbTH1SjzlVrnNA6XlAeH9ehqTewiH_nNM")
-    API_ID = int(os.getenv("API_ID", 18579066))
-    API_HASH = os.getenv("API_HASH", "b6922d525d9c218cc237aa8f6e31e9a6")
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "RukaProBot")
+    TOKEN = os.getenv("TOKEN", "7332398186:AAHG5L3MF-8BtP4ouR_9a_T2tBgje_GegN0")
+    API_ID = int(os.getenv("API_ID", 5047271))
+    API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "testingplate45_bot")
 
     # Database Configuration
     DATABASE_URL = os.getenv("DATABASE_URL", "https://blue-api.vercel.app/database?client=ishikki@xyz242.gramdb")
 
     # Logging and Monitoring
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001631997698"))  # Example: -1001234567890 (Private Channel ID)
-    ERROR_LOG_CHANNEL = int(os.getenv("ERROR_LOG_CHANNEL", -1001552477173))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002325247996"))  # Example: -1001234567890 (Private Channel ID)
+    ERROR_LOG_CHANNEL = int(os.getenv("ERROR_LOG_CHANNEL", -1002325247996))
    
     # Access Control
-    DEV_USERS = list(map(int, os.getenv("DEV_USERS", "5030730429").split(',')))
-    OWNER_ID = int(os.getenv("OWNER_ID", 5030730429))
+    DEV_USERS = list(map(int, os.getenv("DEV_USERS", "1451534504").split(',')))
+    OWNER_ID = int(os.getenv("OWNER_ID", 1451534504))
 
     # Optional Extras
-    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "BotsLabXd")  # Without '@'
-    SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", -1001703076744))
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "hghgjkgjgjk")  # Without '@'
+    SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", -1001720451925))
     
     # Additional API integrations
     MEOWCORE_TOKEN = os.getenv("MEOWCORE_TOKEN", "69696969-MeowMeow")
